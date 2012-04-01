ba06ee721ad3ace69bdfab97e35935852404f2ca
leonardoaraujo.santos <leonardoaraujo.santos@4d54e3ea-7620-4c48-b8f3-07ec67d5f0b5>
2012-04-01 21:35:59 +0000

    Adding ControlUnit skeleton

---
+++ hdl/opencpu32/ControlUnit.vhd
@@ -0,0 +1,46 @@
+--! @file
+--! @brief ControlUnit http://en.wikipedia.org/wiki/Control_unit
+
+--! Use standard library and import the packages (std_logic_1164,std_logic_unsigned,std_logic_arith)
+library IEEE;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+
+--! Use CPU Definitions package
+use work.pkgOpenCPU32.all;
+
+--! The control unit coordinates the input and output devices of a computer system. It fetches the code of all of the instructions \n
+--! in the microprograms. It directs the operation of the other units by providing timing and control signals. \n
+--! all computer resources are managed by the Control Unit.It directs the flow of data between the cpu and the other devices.\n
+--! The outputs of the control unit control the activity of the rest of the device. A control unit can be thought of as a finite-state machine.
+
+--! The purpose of datapaths is to provide routes for data to travel between functional units.
+entity ControlUnit is
+    Port ( reset : in  STD_LOGIC;
+           clk : in  STD_LOGIC;
+           FlagsDp : in  STD_LOGIC_VECTOR (7 downto 0);
+           DataDp : in  STD_LOGIC_VECTOR (7 downto 0);
+           MuxDp : out  STD_LOGIC_VECTOR (2 downto 0);
+           ImmDp : out  STD_LOGIC_VECTOR (7 downto 0);
+           DpRegFileWriteAddr : out  STD_LOGIC;
+           DpRegFileWriteEn : out  STD_LOGIC;
+           DpRegFileReadAddrA : out  STD_LOGIC;
+           DpRegFileReadAddrB : out  STD_LOGIC;
+           DpRegFileReadEnA : out  STD_LOGIC;
+           DpRegFileReadEnB : out  STD_LOGIC;
+           MemoryDataInput : in  STD_LOGIC_VECTOR (7 downto 0);
+           MemoryDataAddr : out  STD_LOGIC_VECTOR (7 downto 0);
+           MemoryDataOut : out  STD_LOGIC_VECTOR (7 downto 0));
+end ControlUnit;
+
+--! @brief ControlUnit http://en.wikipedia.org/wiki/Control_unit
+--! @details The control unit receives external instructions or commands which it converts into a sequence of control signals that the control \n
+--! unit applies to data path to implement a sequence of register-transfer level operations.
+architecture Behavioral of ControlUnit is
+
+begin
+
+
+end Behavioral;
+
---
+++ hdl/opencpu32/testControlUnit.vhd
@@ -0,0 +1,110 @@
+--! @file
+--! @brief Testbench for ControlUnit
+
+--! Use standard library and import the packages (std_logic_1164,std_logic_unsigned,std_logic_arith)
+library IEEE;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+ 
+--! Use CPU Definitions package
+use work.pkgOpenCPU32.all;
+ 
+ENTITY testControlUnit IS
+END testControlUnit;
+ 
+--! @brief ControlUnit Testbench file
+--! @details Exercise the control unit with a assembly program sample
+--! for more information: http://vhdlguru.blogspot.com/2010/03/how-to-write-testbench.html
+ARCHITECTURE behavior OF testControlUnit IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT ControlUnit
+    PORT(
+         reset : IN  std_logic;															
+         clk : IN  std_logic;
+         FlagsDp : IN  std_logic_vector(7 downto 0);
+         DataDp : IN  std_logic_vector(7 downto 0);
+         MuxDp : OUT  std_logic_vector(2 downto 0);
+         ImmDp : OUT  std_logic_vector(7 downto 0);
+         DpRegFileWriteAddr : OUT  std_logic;
+         DpRegFileWriteEn : OUT  std_logic;
+         DpRegFileReadAddrA : OUT  std_logic;
+         DpRegFileReadAddrB : OUT  std_logic;
+         DpRegFileReadEnA : OUT  std_logic;
+         DpRegFileReadEnB : OUT  std_logic;
+         MemoryDataInput : IN  std_logic_vector(7 downto 0);
+         MemoryDataAddr : OUT  std_logic_vector(7 downto 0);
+         MemoryDataOut : OUT  std_logic_vector(7 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal reset : std_logic := '0';															--! Wire to connect Test signal to component
+   signal clk : std_logic := '0';															--! Wire to connect Test signal to component
+   signal FlagsDp : std_logic_vector(7 downto 0) := (others => '0');				--! Wire to connect Test signal to component
+   signal DataDp : std_logic_vector(7 downto 0) := (others => '0');				--! Wire to connect Test signal to component
+   signal MemoryDataInput : std_logic_vector(7 downto 0) := (others => '0');	--! Wire to connect Test signal to component
+
+ 	--Outputs
+   signal MuxDp : std_logic_vector(2 downto 0);											--! Wire to connect Test signal to component
+   signal ImmDp : std_logic_vector(7 downto 0);											--! Wire to connect Test signal to component
+   signal DpRegFileWriteAddr : std_logic;													--! Wire to connect Test signal to component
+   signal DpRegFileWriteEn : std_logic;													--! Wire to connect Test signal to component
+   signal DpRegFileReadAddrA : std_logic;													--! Wire to connect Test signal to component
+   signal DpRegFileReadAddrB : std_logic;													--! Wire to connect Test signal to component
+   signal DpRegFileReadEnA : std_logic;													--! Wire to connect Test signal to component
+   signal DpRegFileReadEnB : std_logic;													--! Wire to connect Test signal to component
+   signal MemoryDataAddr : std_logic_vector(7 downto 0);								--! Wire to connect Test signal to component
+   signal MemoryDataOut : std_logic_vector(7 downto 0);								--! Wire to connect Test signal to component
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	--! Instantiate the Unit Under Test (ControlUnit)
+   uut: ControlUnit PORT MAP (
+          reset => reset,
+          clk => clk,
+          FlagsDp => FlagsDp,
+          DataDp => DataDp,
+          MuxDp => MuxDp,
+          ImmDp => ImmDp,
+          DpRegFileWriteAddr => DpRegFileWriteAddr,
+          DpRegFileWriteEn => DpRegFileWriteEn,
+          DpRegFileReadAddrA => DpRegFileReadAddrA,
+          DpRegFileReadAddrB => DpRegFileReadAddrB,
+          DpRegFileReadEnA => DpRegFileReadEnA,
+          DpRegFileReadEnB => DpRegFileReadEnB,
+          MemoryDataInput => MemoryDataInput,
+          MemoryDataAddr => MemoryDataAddr,
+          MemoryDataOut => MemoryDataOut
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+      wait for clk_period*10;
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
