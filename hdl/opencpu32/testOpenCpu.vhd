2a088e387ff67ab76563da54a6c8f4e61d1a74b0
leonardoaraujo.santos <leonardoaraujo.santos@4d54e3ea-7620-4c48-b8f3-07ec67d5f0b5>
2012-04-15 22:41:00 +0000

    Working a little bit on documentation

--- hdl/opencpu32/testOpenCpu.vhd
+++ hdl/opencpu32/testOpenCpu.vhd
@@ -10,11 +10,14 @@ use ieee.std_logic_arith.all;
 --! Use CPU Definitions package
 use work.pkgOpenCPU32.all;
 
---! Adding library for File I/O (Synposys Text I/O package)
+--! Adding library for File I/O 
+-- More information on this site:
+-- http://eesun.free.fr/DOC/vhdlref/refguide/language_overview/test_benches/reading_and_writing_files_with_text_i_o.htm
 use std.textio.ALL;
 use ieee.std_logic_textio.all;
  
 ENTITY testOpenCpu IS
+generic (n : integer := nBits - 1);											--! Generic value (Used to easily change the size of the Alu on the package)
 END testOpenCpu;
  
 --! @brief openCpu Testbench file
@@ -23,37 +26,37 @@ ARCHITECTURE behavior OF testOpenCpu IS
  
     --! Component declaration to instantiate the Multiplexer circuit			
     COMPONENT openCpu
-    PORT(
-         rst : IN  std_logic;
-         clk : IN  std_logic;
-         mem_rd : OUT  std_logic;
-         mem_rd_addr : OUT  std_logic_vector(31 downto 0);
-         mem_wr : OUT  std_logic;
-         mem_wr_addr : OUT  std_logic_vector(31 downto 0);
-         mem_data_in : IN  std_logic_vector(31 downto 0);
-         mem_data_out : OUT  std_logic_vector(31 downto 0)
-        );
+    generic (n : integer := nBits - 1);									--! Generic value (Used to easily change the size of the Alu on the package)
+	 Port ( rst : in  STD_LOGIC;												--! Reset signal
+           clk : in  STD_LOGIC;												--! Clock signal
+           mem_rd : out  STD_LOGIC;											--! Main memory Read enable
+           mem_rd_addr : out  STD_LOGIC_VECTOR (n downto 0);		--! Main memory Read address
+           mem_wr : out  STD_LOGIC;											--! Main memory Write enable
+           mem_wr_addr : out  STD_LOGIC_VECTOR (n downto 0);		--! Main memory Write address
+			  mem_data_in : in  STD_LOGIC_VECTOR (n downto 0);			--! Data comming from main memory
+			  mem_data_out : out  STD_LOGIC_VECTOR (n downto 0)		--! Data to main memory
+			  );
     END COMPONENT;
     
 
    --Inputs
-   signal rst : std_logic := '0';
-   signal clk : std_logic := '0';
-   signal mem_data_in : std_logic_vector(31 downto 0) := (others => '0');
+   signal rst : std_logic := '0';														--! Wire to connect Test signal to component
+   signal clk : std_logic := '0';														--! Wire to connect Test signal to component
+   signal mem_data_in : std_logic_vector(n downto 0) := (others => '0');	--! Wire to connect Test signal to component
 
  	--Outputs
-   signal mem_rd : std_logic;
-   signal mem_rd_addr : std_logic_vector(31 downto 0);
-   signal mem_wr : std_logic;
-   signal mem_wr_addr : std_logic_vector(31 downto 0);
-   signal mem_data_out : std_logic_vector(31 downto 0);
+   signal mem_rd : std_logic;																--! Wire to connect Test signal to component
+   signal mem_rd_addr : std_logic_vector(n downto 0);								--! Wire to connect Test signal to component
+   signal mem_wr : std_logic;																--! Wire to connect Test signal to component
+   signal mem_wr_addr : std_logic_vector(n downto 0);								--! Wire to connect Test signal to component
+   signal mem_data_out : std_logic_vector(n downto 0);							--! Wire to connect Test signal to component
 
    -- Clock period definitions
    constant clk_period : time := 10 ns;
  
 BEGIN
  
-	--!Instantiate the Unit Under Test (openCpu) (Doxygen bug if it's not commented!)
+	--! Instantiate the Unit Under Test (openCpu) (Doxygen bug if it's not commented!)
    uut: openCpu PORT MAP (
           rst => rst,
           clk => clk,
